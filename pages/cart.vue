a1bc3391ae1419fd3be0a85754ea4a8bcea7b836
Rajnish <razz.king2004@gmail.com>
2023-12-29 18:52:23 +0530

    sekeleton loding component  add

--- pages/cart.vue
+++ pages/cart.vue
@@ -6,10 +6,11 @@
                   <!-- cart product list container-->
                   <div class="rounded-lg md:w-2/3">
                         <!-- cart product -->
-                        <div v-for="item , i in cartItems.cartItems" :key="i" class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+                        <div v-if="cartItems.cartItems.length" v-for="item, i in cartItems.cartItems" :key="i"
+                              class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
                               <!-- product img -->
-                              <img :src="item.image"
-                                    alt="product-image" class="w-full h-32 rounded-lg sm:w-40 object-contain" />
+                              <img :src="item.image" alt="product-image"
+                                    class="w-full h-32 rounded-lg sm:w-40 object-contain" />
                               <!-- END -->
                               <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
                                     <div class="mt-5 sm:mt-0">
@@ -19,21 +20,18 @@
                                     <!-- Quantity , remove and price -->
                                     <div class="mt-4 sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                                           <div class=" border-gray-100">
-                                                <span
-                                                      @click="cartItems.subQnt(i)"
+                                                <span @click="cartItems.subQnt(i)"
                                                       class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
                                                       - </span>
                                                 <input class="h-8 w-8 border bg-white text-center text-xs outline-none"
                                                       type="number" :value="item.quantity" min="1" />
-                                                <span
-                                                      @click="cartItems.addQnt(i)"
+                                                <span @click="cartItems.addQnt(i)"
                                                       class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
                                                       + </span>
                                           </div>
                                           <div>
                                                 <p class="text-sm font-bold">$ {{ item.price }} </p>
-                                                <button
-                                                      @click="cartItems.removeCartItem(i)"
+                                                <button @click="cartItems.removeCartItem(i)"
                                                       class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
                                                       Remove
                                                 </button>
@@ -43,6 +41,34 @@
                               </div>
                         </div>
 
+                        <!-- when cart is empty -->
+                        <div v-else
+                              class="flex flex-col w-full gap-5 p-2 mx-auto bg-white shadow-lg select-none sm:p-4 sm:h-64 rounded-2xl sm:flex-row ">
+                              <div class="bg-gray-200 h-52 sm:h-full sm:w-72 rounded-xl animate-pulse">
+                              </div>
+                              <div class="flex flex-col flex-1 gap-5 sm:p-2">
+                                    <div class="flex flex-col flex-1 gap-3">
+                                          <div class="w-full bg-gray-200 animate-pulse h-14 rounded-2xl">
+                                          </div>
+                                          <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                                          </div>
+                                          <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                                          </div>
+                                          <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                                          </div>
+                                          <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                                          </div>
+                                    </div>
+                                    <div class="flex gap-3 mt-auto">
+                                          <div class="w-20 h-8 bg-gray-200 rounded-full animate-pulse">
+                                          </div>
+                                          <div class="w-20 h-8 bg-gray-200 rounded-full animate-pulse">
+                                          </div>
+                                          <div class="w-20 h-8 ml-auto bg-gray-200 rounded-full animate-pulse">
+                                          </div>
+                                    </div>
+                              </div>
+                        </div>
                   </div>
                   <!-- Sub total -->
                   <div class="bg-fixed mt-6 h-full rounded-lg border bg-white p-6 pb-10 shadow-md md:mt-0 md:w-1/3">
@@ -66,12 +92,8 @@
                               class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check
                               out
                         </button>
-                        <button
-                
-                type="button"
-                class="bg-blue-400 fixed bottom-16 rounded-full grid place-items-center transition-all duration-300 right-[20px] z-90 w-[48px] h-[48px]"
-
-              ></button>
+                        <button type="button"
+                              class="bg-blue-400 fixed bottom-16 rounded-full grid place-items-center transition-all duration-300 right-[20px] z-90 w-[48px] h-[48px]"></button>
                   </div>
             </div>
       </div>
@@ -82,7 +104,7 @@ import { useCartStore } from '~/store/cartStore';
 
 
 const cartItems = useCartStore()
-const shipping = computed(()=> +(cartItems.totalPrice / 6).toFixed(2) )
-const subtotal = computed(()=> +(cartItems.totalPrice).toFixed(2) )
+const shipping = computed(() => +(cartItems.totalPrice / 6).toFixed(2))
+const subtotal = computed(() => +(cartItems.totalPrice).toFixed(2))
 
 </script>
